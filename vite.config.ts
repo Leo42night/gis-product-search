c74609723d7453af1b8985b14ce9853c4f84d953
Leo42night <109854127+Leo42night@users.noreply.github.com>
2024-12-09 15:22:07 +0700

    perbaiki vite https

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
 import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
+    https: process.env.VITE_NODE_ENV === 'development' ? {
       key: fs.readFileSync(path.resolve(__dirname, 'myapp-privateKey.key')),
       cert: fs.readFileSync(path.resolve(__dirname, 'myapp.crt')),
-    },
+    } : undefined, // Nonaktifkan HTTPS di production
   },
 })
